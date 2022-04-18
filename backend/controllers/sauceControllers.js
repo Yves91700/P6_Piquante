c86e8d70b174fa905f557a31e8300b3a1fbb6488
Yves91700 <yvespierre91700@gmail.com>
2022-04-18 23:44:53 +0200

    const fse remis en const fspb de suppression sauce

--- backend/controllers/sauceControllers.js
+++ backend/controllers/sauceControllers.js
@@ -1,65 +1,81 @@
-const Sauce = require('../models/sauce');
-const fse = require('fs-extra'); //Package pour accéder et interagir avec le système de fichiers
-//**************** logique metier************** */
+/**************** Déclarations et Importations***************** */
+
+const Sauce = require("../models/sauce");
+const fs = require("fs"); //Package pour accéder et interagir avec le système de fichiers
+
+/********************** logique metier ************************************* */
+
+/***********Création d'une nouvelle sauce ************ */
 
 exports.createSauce = (req, res, next) => {
-  const sauceObject = JSON.parse(req.body.sauce)
+  const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
   const sauce = new Sauce({
     ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
     likes: 0,
     dislikes: 0,
-    usersLiked: [' '],
-    usersdisLiked: [' '],
+    usersLiked: [" "],
+    usersdisLiked: [" "],
   });
+  //enregister la sauce dans la base de donnée en appelant la méthode save
   sauce
     .save()
     .then(() => res.status(201).json({ message: "Sauce enregistrée" }))
     .catch((error) => res.status(400).json({ error }));
 };
 
-
+/*********** Modification d'une sauce *********** */
 exports.updateSauce = (req, res, next) => {
-  const sauceObject = req.file ?
-  {
-    ...JSON.parse(req.body.sauce),
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`} : { ...req.body }
+  const sauceObject = req.file
+    ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
 
-  Sauce.updateOne({ _id : req.params.id}, {...sauceObject, _id: req.params.id})
-  .then(res.status(200).json({ message : "Sauce modifiée"}))
-  .catch(error => res.status(400).json({ error }))
-}; 
+  Sauce.updateOne(
+    { _id: req.params.id },
+    { ...sauceObject, _id: req.params.id }
+  )
+    .then(res.status(200).json({ message: "Sauce modifiée" }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
+/********** Récupération de toutes les sauces ************ */
 
 exports.getAllSauces = (req, res, next) => {
-    Sauce.find()
-      .then((sauces) => res.status(200).json(sauces))
-      .catch((error) => res.status(404).json({ error }));
-  };
+  // utilisation de la méthode find() pour avoir la liste complete
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(404).json({ error }));
+};
 
-  exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-      .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(404).json({ error }));
-  };
+/********* Récuperation d'une seul sauce ************** */
 
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id }) // nous utilisons l'ID que nous recevons en paramètres pour accéder à la Sauce correspondant dans la BDD
 
-  exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({ _id : req.params.id })
-    .then(sauce => {
-      const filename = sauce.imageUrl.split("/images/")[1]
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
+
+/********** Suppression d'une sauce ********************** */
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id }) // nous utilisons l'ID que nous recevons en paramètres pour accéder à la Sauce correspondant dans la BDD
+
+    .then((sauce) => {
+      const filename = sauce.imageUrl.split("/images/")[1]; // On récupère avec la méthode split le nom ficher image dans l'URL
+
       fs.unlink(`images/${filename}`, () => {
-        Sauce.deleteOne({_id : req.params.id})
-    .then(res.status(200).json({ message: "Sauce supprimée" }))
-    .catch(error => res.status(400).json({ error }))
-      })
-    })
-    .catch(error => res.status(500).json({ error }))
-    
-    /*if (thing.userId !== req.auth.userId) {
-      res.status(400).json({
-        error: new Error('Unauthorized request!')
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(res.status(200).json({ message: "Sauce supprimée" }))
+          .catch((error) => res.status(400).json({ error }));
       });
-    }*/
-  };
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
